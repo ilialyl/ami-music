4dd70b1564677570330bf5280562944817adbf50
lyns0 <disse.sl@proton.me>
2026-07-31 13:01:39 +0700

    feat: add more buttons to control bar

--- web-ui/src/lib/components/ControlBar.svelte
+++ web-ui/src/lib/components/ControlBar.svelte
@@ -3,42 +3,38 @@
 	import * as queue from '$lib/commands/queue';
 	import * as library from '$lib/commands/library';
 	import { daemonState } from '$lib/stores/daemon_states.svelte';
+	import { Pause, Play, SkipBack, SkipForward } from '@lucide/svelte';
 
 	let currentTrack = $derived(daemonState.queue?.current_track);
+
+	let time = $derived(daemonState.player?.position.secs != null
+	  ? `${Math.floor(daemonState.player.position.secs / 60)}:${(daemonState.player.position.secs % 60).toString().padStart(2, '0')}`
+	  : "0:00")
 </script>
 
 <div
 	class="flex h-1/12 shrink-0 flex-row items-center justify-between space-x-5 border-t-2 bg-black px-10 text-white"
 >
-	<div>
-		<button class="cursor-pointer" onclick={playback.play} aria-label="Play"
-			><svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="size-6"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
-				/>
-			</svg>
-		</button>
-		<button class="cursor-pointer" onclick={playback.pause} aria-label="Pause"
-			><svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="size-6"
-			>
-				<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
-			</svg>
-		</button>
+    <div>
+        {time}
+    </div>
+	<div class="flex space-x-5">
+    	<button class="cursor-pointer" onclick={queue.prev}>
+    	    <SkipBack strokeWidth={1} />
+      	</button>
+
+	    {#if daemonState.player?.playback_status === "Paused"}
+    		<button class="cursor-pointer" onclick={playback.play}>
+    		    <Play strokeWidth={1} />
+    		</button>
+        {:else}
+            <button class="cursor-pointer" onclick={playback.pause}>
+      		    <Pause strokeWidth={1} />
+      		</button>
+		{/if}
+		<button class="cursor-pointer" onclick={queue.next}>
+      	    <SkipForward strokeWidth={1} />
+      	</button>
 	</div>
 	<div>
 		<h1 class="text-lg font-bold">
